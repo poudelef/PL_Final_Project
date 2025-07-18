333628566aa2374e4bf21530c1613b015e591933
sambhavpoudel88@gmail.com <youremail@example.com>
2025-07-18 18:54:15 -0400

    Workrd on appartmentList

---
+++ backend/apps/modules/appartmentListing.py
@@ -0,0 +1,10 @@
+class apparmentList():
+    def __init__(self, landLord_name=None, location=None, price=0, bedrooms=None, bathrooms = None, kitchen=None, balcony=None, Hall = False):
+        self.name = landLord_name
+        self.location = location
+        self.price = price
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.kitchen = kitchen
+        self.balcony = balcony
+        self.hall = Hall
